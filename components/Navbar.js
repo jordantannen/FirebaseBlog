7833e502f2f2ca3b6a4be1fd44300562e418d1d2
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-18 20:23:04 -0500

    Add nav bar

---
+++ components/Navbar.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+export default function Navbar() {
+    const { user, username } = {};
+
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button className="btn-logo">Fire Base Blog</button>
+                    </Link>
+                </li>
+
+                { username && (
+                    <>
+                        <li className="push-left">
+                            <Link href="/admin">
+                                <button className="btn-blue">Write Posts</button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={`/${username}`}>
+                                <img src={user?.photoURL}/>
+                            </Link>
+                        </li>
+                    </>
+                )}
+
+                { !username && (
+                    <li>
+                        <Link href="/enter">
+                        <button className="btn-blue">Log in</button>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
