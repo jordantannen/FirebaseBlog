150a4075451e6abd560d326bb4895d08d26939a7
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-18 20:27:37 -0500

    Add Toast messages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import Loader from '../components/Loader'
+import { toast, Toast } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast
+      </button>
     </>
   )
 }
