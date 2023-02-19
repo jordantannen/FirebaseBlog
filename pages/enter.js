26bb5bc1e8d5fba1552800a10c6b82761416a147
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-19 13:16:53 -0500

    Add debounce, username check

--- pages/enter.js
+++ pages/enter.js
@@ -1,6 +1,8 @@
 import { auth, googleAuthProvider } from "../lib/firebase"
-import { useContext } from "react"
+import { useContext, useEffect, useState, useCallback } from "react"
 import { UserContext } from '../lib/context'
+import debounce from 'lodash.debounce';
+
 
 export default function EnterPage(props) {
     const { user, username } = useContext(UserContext)
@@ -44,8 +46,68 @@ export default function EnterPage(props) {
     }
 
     function UsernameForm() {
+        const [formValue, setFormValue] = useState('')
+        const [isValid, setIsValid] = useState(false)
+        const [loading, setLoading] = useState(false)
+
+        const {user, username} = useContext(UserContext)
+
+        const onChange = (e) => {
+            const val = e.target.value.toLowerCase();
+            const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+            if (val.length < 3) {
+                setFormValue(val);
+                setLoading(false);
+                setIsValid(false);
+            }
+
+            if (re.test(val)) {
+                setFormValue(val)
+                setLoading(true)
+                setIsValid(false)
+            }
+        }
+
+        useEffect(() => {checkUsername(formValue), [formValue]})
+
+        const checkUsername = useCallback(
+            debounce(async (username) => {
+              if (username.length >= 3) {
+                const ref = firestore.doc(`usernames/${username}`);
+                const { exists } = await ref.get();
+                console.log('Firestore read executed!');
+                setIsValid(!exists);
+                setLoading(false);
+              }
+            }, 500),
+            []
+          );
+
         return (
-            <h1>form</h1>
+            !username && (
+                <section>
+                    <h3>Select Username</h3>
+                    <form onSubmit={onSumbit}>
+                        <input name="username" placeholder="username" value={formValue} onChange={onChange}/>
+                        <button type="summit" className="btn-green" disabled={!isValid}>
+                            Submit
+                        </button>
+                    </form>
+                </section>
+            )
         );
+    }
+}
+
+function UsernameMessage({ username, isValid, loading }) {
+    if (loading) {
+      return <p>Checking...</p>;
+    } else if (isValid) {
+      return <p className="text-success">{username} is available!</p>;
+    } else if (username && !isValid) {
+      return <p className="text-danger">That username is taken!</p>;
+    } else {
+      return <p></p>;
     }
 }
