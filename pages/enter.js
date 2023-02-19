c5604345afe58fb8bbdd544c4e3052385f18c97a
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-18 21:51:58 -0500

    Add Google Authentication

--- pages/enter.js
+++ pages/enter.js
@@ -1,11 +1,34 @@
-export default function EnterPage({ }) {
+import { auth, googleAuthProvider } from "../lib/firebase"
+
+export default function EnterPage(props) {
+    const user = null
+    const username = null
+
     return (
         <main>
-            <h1>Sign Up</h1>
+            {/* If user signed out, show sign in button,  signed out, show signout button,
+            signed in but no user name, requests the username form */}
+            {user ? !username ? <UsernameForm/> : <SignOutButton/> : <SignInButton/> }
         </main>
     )
 
-    function SignInButton(){
-        
+    function SignInButton() {
+        const signInWithGoogle = async () => {
+            await auth.signInWithPopup(googleAuthProvider)
+        }
+
+        return (
+            <button className="btn-google" onClick={signInWithGoogle}>
+                Sign in with Google
+            </button>
+        )
+    }
+
+    function SignOutButton() {
+        return <button onClick={() => auth.signOut()}>Sign Out</button>
+    }
+
+    function UsernameForm() {
+        return null;
     }
 }
