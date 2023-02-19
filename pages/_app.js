86204cfa83a700f8492421691aed0656ed50032f
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-18 22:04:14 -0500

    Convert to JS

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '@/styles/globals.css'
+import Navbar from '../components/Navbar'
+import { Toaster } from 'react-hot-toast'
+import { UserContext } from '../lib/context'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <UserContext.Provider value={{user: {}, username: 'Jordan'}}>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Toaster/>
+    </UserContext.Provider>
+  )
+}
