a7ea52c247ca7991f3a75c91f17de0365b0b1d68
Jordan Tannenbaum <jordantannen@gmail.com>
2023-02-18 20:23:07 -0500

    Add toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast'
+import Navbar from '../components/Navbar'
+import { Toast } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar/>
+      <Component {...pageProps} />
+      <Toaster/>
+    </>
+  )
 }
